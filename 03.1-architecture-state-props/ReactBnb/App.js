6185d07486bd9330e45ec9509c97b589a17fc1fc
thomas <tf@kumquats.fr>
2017-12-18 16:50:08 +0100

    03.1 : demarrage

---
+++ 03.1-architecture-state-props/ReactBnb/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          ReactBnB
+        </Text>
+        <View>
+          <Text>Maison entière - 3 lits</Text>
+          <Text>Havre de paix en bord de mer</Text>
+          <Text>112 euros / nuits</Text>
+          <Text>Note: 5/5</Text>
+        </View>
+        <View>
+          <Text>Chambre - 1 lit double</Text>
+          <Text>Appartement proche du centre ville</Text>
+          <Text>45 euros / nuits</Text>
+          <Text>Note: 3/5</Text>
+        </View>
+        <View>
+          <Text>Chambre - 1 lit simple</Text>
+          <Text>Maison proche des transports</Text>
+          <Text>64 euros / nuits</Text>
+          <Text>Note: 1/5</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
