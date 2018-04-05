48a91eb91721641d7ece031d2f032367ff44cd44
thomas <tf@kumquats.fr>
2018-04-06 00:54:35 +0200

    07 : maj react-navigation

--- 07-usages-avances/ReactBnb/src/store.js
+++ 07-usages-avances/ReactBnb/src/store.js
@@ -1,11 +1,20 @@
 import { createStore, compose, applyMiddleware } from "redux";
-import reducer from "./reducers";
+import { createReactNavigationReduxMiddleware } from "react-navigation-redux-helpers";
 import thunk from 'redux-thunk';
+import reducer from "./reducers";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function configureStore(){
-    return createStore( reducer, composeEnhancers(
-        applyMiddleware(thunk)
-    ) );
+	const navMiddleware = createReactNavigationReduxMiddleware(
+		'root',
+		state => state.nav,
+	);
+
+    return createStore(
+		reducer,
+		composeEnhancers(
+        	applyMiddleware(thunk, navMiddleware)
+		)
+	);
 }
