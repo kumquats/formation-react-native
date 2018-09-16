71be608ef52c2127a23b64ad3290389f2b2d6a14
thomas <tf@kumquats.fr>
2018-09-16 21:55:46 +0200

    maj react-native 0.57.0

--- 03.1-architecture-state-props/ReactBnb/App.js
+++ 03.1-architecture-state-props/ReactBnb/App.js
@@ -1,9 +1,5 @@
-import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default class App extends Component {
   render() {
