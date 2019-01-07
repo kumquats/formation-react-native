a534cc7ae15717d91f73e5e33f32a18e07d1fdb6
thomas <tf@kumquats.fr>
2019-01-07 14:06:03 +0100

    04.2 : maj Navigator avec la notation raccourcie (shorthand)

--- 05-formulaire-et-gestion-des-donnees/ReactBnb/src/containers/Navigator.js
+++ 05-formulaire-et-gestion-des-donnees/ReactBnb/src/containers/Navigator.js
@@ -5,17 +5,8 @@ import HousingList from './HousingList';
 import HousingDetail from './HousingDetail';
 
 const Navigator = createStackNavigator({
-    list: {
-        screen: HousingList,
-        path: 'list'
-    },
-    detail: {
-      screen: HousingDetail,
-      path: 'detail/:id'
-    },
-    search: {
-        screen: SearchForm,
-        path: 'search'
-    }
+    list: HousingList,
+    detail: HousingDetail,
+    search: SearchForm
 });
 export default Navigator;
--- 07-usages-avances/ReactBnb/src/containers/Navigator.js
+++ 07-usages-avances/ReactBnb/src/containers/Navigator.js
@@ -3,24 +3,10 @@ import { createStackNavigator } from 'react-navigation';
 import SearchForm from './SearchForm';
 import HousingList from './HousingList';
 import HousingDetail from './HousingDetail';
-import CreateForm from './CreateForm';
 
 const Navigator = createStackNavigator({
-    list: {
-        screen: HousingList,
-        path: 'list'
-    },
-    detail: {
-      screen: HousingDetail,
-      path: 'detail/:id'
-    },
-    search: {
-        screen: SearchForm,
-        path: 'search'
-	},
-    create: {
-      screen: CreateForm,
-      path: 'create'
-    }
+    list: HousingList,
+    detail: HousingDetail,
+    search: SearchForm,
 });
 export default Navigator;
