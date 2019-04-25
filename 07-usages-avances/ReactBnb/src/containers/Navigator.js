eabec5887c207770fddf1f6817d24170918feb2f
thomas <tf@kumquats.fr>
2019-04-26 00:08:56 +0200

    07 : nettoyage Navigator (reprise 06)

--- 07-usages-avances/ReactBnb/src/containers/Navigator.js
+++ 07-usages-avances/ReactBnb/src/containers/Navigator.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import SearchForm from './SearchForm';
 import HousingList from './HousingList';
@@ -7,6 +6,6 @@ import HousingDetail from './HousingDetail';
 const Navigator = createStackNavigator({
     list: HousingList,
     detail: HousingDetail,
-    search: SearchForm,
+    search: SearchForm
 });
 export default createAppContainer( Navigator );
