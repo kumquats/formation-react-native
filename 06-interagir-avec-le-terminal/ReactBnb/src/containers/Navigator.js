c2b0e276434a934bf87b1ebfa88998589dd659dd
thomas <tf@kumquats.fr>
2019-04-25 23:38:54 +0200

    06 : correction navigator : page par défaut = liste

--- 06-interagir-avec-le-terminal/ReactBnb/src/containers/Navigator.js
+++ 06-interagir-avec-le-terminal/ReactBnb/src/containers/Navigator.js
@@ -1,12 +1,11 @@
-import React from 'react';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import SearchForm from './SearchForm';
 import HousingList from './HousingList';
 import HousingDetail from './HousingDetail';
 
 const Navigator = createStackNavigator({
-    search: SearchForm,
     list: HousingList,
     detail: HousingDetail,
+    search: SearchForm
 });
 export default createAppContainer( Navigator );
